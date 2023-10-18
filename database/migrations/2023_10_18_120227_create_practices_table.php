cc0d7e31d9bace434c41ac8a0fee43fb18704dfd
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-18 15:45:08 +0330

    add practice table

--- database/migrations/2023_10_18_120227_create_practices_table.php
+++ database/migrations/2023_10_18_120227_create_practices_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Topic;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('practices', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Topic::class)->constrained();
+            $table->string('title');
+            $table->text('body');
+            $table->text('answer');
             $table->timestamps();
         });
     }
