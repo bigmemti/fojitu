1863ddfdd9f30f32d6c3870382fd22689bd675a0
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-18 15:31:35 +0330

    add homework model files

---
+++ app/Http/Controllers/HomeWorkController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HomeWork;
+use App\Http\Requests\StoreHomeWorkRequest;
+use App\Http\Requests\UpdateHomeWorkRequest;
+
+class HomeWorkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHomeWorkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HomeWork $homeWork)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HomeWork $homeWork)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHomeWorkRequest $request, HomeWork $homeWork)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HomeWork $homeWork)
+    {
+        //
+    }
+}
