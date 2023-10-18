565483cb445988f878e0028dd4e81d4d7d721960
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-18 14:46:18 +0330

    add form styles

--- resources/views/session/create.blade.php
+++ resources/views/session/create.blade.php
@@ -9,14 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action="{{route('course.session.store', ['course' => $course])}}" method="POST">
+                    <form class="flex flex-col gap-4" action="{{route('course.session.store', ['course' => $course])}}" method="POST">
                         @csrf
-                        <label for="name">name:</label>
-                        <input type="text" name="name" id="name" value="{{old('name')}}">
+                        <label class="text-xl font-bold" for="name">name:</label>
+                        <input class="dark:bg-black dark:text-white" type="text" name="name" id="name" value="{{old('name')}}">
                         @error('name')
                             <span>{{$message}}</span>
                         @enderror
-                        <button type="submit">submit</button>
+                        <button class="self-end mx-2 px-3 py-2 bg-green-500 hover:bg-green-400 dark:bg-green-800 dark:hover:bg-green-700 rounded-xl" type="submit">submit</button>
                     </form>
                 </div>
             </div>
--- resources/views/session/edit.blade.php
+++ resources/views/session/edit.blade.php
@@ -9,15 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action="{{route('session.update', ['session' => $session])}}" method="POST">
+                    <form class="flex flex-col gap-4" action="{{route('session.update', ['session' => $session])}}" method="POST">
                         @csrf
                         @method('patch')
-                        <label for="name">name:</label>
-                        <input type="text" name="name" id="name" value="{{$session->name}}">
+                        <label class="text-xl font-bold" for="name">name:</label>
+                        <input class="dark:bg-black dark:text-white" type="text" name="name" id="name" value="{{$session->name}}">
                         @error('name')
                             <span>{{$message}}</span>
                         @enderror
-                        <button type="submit">submit</button>
+                        <button class="self-end mx-2 px-3 py-2 bg-green-500 hover:bg-green-400 dark:bg-green-800 dark:hover:bg-green-700 rounded-xl" type="submit">submit</button>
                     </form>
                 </div>
             </div>
