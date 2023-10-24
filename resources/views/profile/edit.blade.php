8e80eccd943a86716d6da66f4871b3dd44f37741
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-24 18:51:28 +0330

    add student request form

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -27,6 +27,13 @@
                 </div>
             @endif
 
+
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.student-form')
+                </div>
+            </div>
+
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
