3039d7572dd4b6dc21856ec78689ac65e52e8b90
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-26 03:15:37 +0330

    add file manage module

---
+++ app/Http/Controllers/MimeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mime;
+use App\Http\Requests\StoreMimeRequest;
+use App\Http\Requests\UpdateMimeRequest;
+
+class MimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMimeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mime $mime)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Mime $mime)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMimeRequest $request, Mime $mime)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mime $mime)
+    {
+        //
+    }
+}
