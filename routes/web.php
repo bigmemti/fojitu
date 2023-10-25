95e74a4535a4b625e8d3de57127db1e0935e89aa
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-26 01:55:49 +0330

    change routes sort

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('user.teacher', TeacherController::class)->shallow();
+    Route::resource('user.student', StudentController::class)->shallow();
+
+    Route::resource('university', UniversityController::class);
+    Route::resource('major', MajorController::class);
+    Route::resource('curriculum', CurriculumController::class);
 
     Route::resource('teacher.course', CourseController::class)->shallow();
     Route::get('/course', [CourseListController::class, 'index'])->name('course.index');
@@ -47,17 +53,10 @@ Route::middleware('auth')->group(function () {
     Route::resource('session.topic', TopicController::class,['except' => ['index']])->shallow();
     Route::resource('session.homework', HomeWorkController::class,['except' => ['index']])->shallow();
     Route::resource('topic.practice', PracticeController::class,['except' => ['index']])->shallow();
+    Route::resource('session.attendance', AttendanceController::class)->shallow();
 
-    Route::resource('user.teacher', TeacherController::class)->shallow();
-    Route::resource('user.student', StudentController::class)->shallow();
-
-
-    Route::resource('university', UniversityController::class);
-    Route::resource('major', MajorController::class);
-    Route::resource('curriculum', CurriculumController::class);
 
     Route::resource('course.member', MemberController::class)->shallow();
-    Route::resource('session.attendance', AttendanceController::class)->shallow();
 });
 
 require __DIR__.'/auth.php';
