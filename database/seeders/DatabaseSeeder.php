d173c17dbfcaff2a5727a6bba158d14bdbd7d48e
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-27 18:00:53 +0330

    off seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,29 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        // $this->call([
+        //     UniversitySeeder::class,
+        //     MajorSeeder::class,
+        //     CurriculumSeeder::class,
+        //     UserSeeder::class,
         // ]);
 
-        $this->call([
-            UserPrimarySeeder::class,
-            UniversitySeeder::class,
-            MajorSeeder::class,
-            CurriculumSeeder::class,
-            UserSeeder::class,
-        ]);
+        // $users = User::inRandomOrder()->get();
+        // $users = $users->chunk(280);
 
-        $users = User::inRandomOrder()->get();
-        $users = $users->chunk(280);
+        // $this->call(StudentSeeder::class,false,['users' => $users[0]]);
+        // $this->call(TeacherSeeder::class,false,['users' => $users[1]]);
 
-        $this->call(StudentSeeder::class,false,['users' => $users[0]]);
-        $this->call(TeacherSeeder::class,false,['users' => $users[1]]);
+        // $this->call([
+        //     CourseSeeder::class,
+        // ]);
 
-        $this->call([
-            CourseSeeder::class,
-        ]);
+        $this->call(UserPrimarySeeder::class);
     }
 }
