270dcd5b938f1a2144bf7c91fb74aec2390c78aa
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-31 13:39:50 +0330

    add some factories

---
+++ database/factories/TeacherRequestFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TeacherRequest>
+ */
+class TeacherRequestFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
