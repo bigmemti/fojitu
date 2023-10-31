cb3bb5430e9340c6e3bddcd47164effa52e41ba0
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-31 13:45:08 +0330

    add editor dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,6 +14,55 @@ const Toast = Swal.mixin({
         toast.addEventListener('mouseleave', Swal.resumeTimer)
     }
 })
+import { Editor } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
+import '@majidh1/jalalidatepicker'
+
+
+document.addEventListener('alpine:init', () => {
+  Alpine.data('editor', (content) => {
+    let editor
+
+    return {
+      updatedAt: Date.now(), // force Alpine to rerender on selection change
+      init() {
+        const _this = this
+
+        editor = new Editor({
+          element: this.$refs.element,
+          extensions: [
+            StarterKit
+          ],
+          content: content,
+          onCreate({ editor }) {
+            _this.updatedAt = Date.now()
+          },
+          onUpdate({ editor }) {
+            _this.updatedAt = Date.now()
+          },
+          onSelectionUpdate({ editor }) {
+            _this.updatedAt = Date.now()
+          }
+        })
+      },
+      isLoaded() {
+        return editor
+      },
+      isActive(type, opts = {}) {
+        return editor.isActive(type, opts)
+      },
+      toggleHeading(opts) {
+        editor.chain().toggleHeading(opts).focus().run()
+      },
+      toggleBold() {
+        editor.chain().toggleBold().focus().run()
+      },
+      toggleItalic() {
+        editor.chain().toggleItalic().focus().run()
+      },
+    }
+  })
+})
 
 window.Alpine = Alpine;
 window.Swal = Swal;
