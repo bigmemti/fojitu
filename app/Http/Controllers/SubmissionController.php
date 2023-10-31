432d9dce135e964e0ba2ed1f17da2c198f50a5e5
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-10-31 04:44:10 +0330

    add index, create and store to submission controller

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
+use App\Models\HomeWork;
 use App\Models\Submission;
+use App\Services\FileService;
 use App\Http\Requests\StoreSubmissionRequest;
 use App\Http\Requests\UpdateSubmissionRequest;
 
@@ -11,25 +14,42 @@ class SubmissionController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(HomeWork $homework)
     {
-        //
+        return view("submission.index",[
+            "homework"=> $homework->load(['members.student.user'])
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(HomeWork $homework)
     {
-        //
+        return view("submission.create",[
+            "homework"=> $homework->load(['types.mimes', 'submissions' => fn($query) => $query->whereRelation('member.student.user', 'users.id', auth()->user()->id)])
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreSubmissionRequest $request)
+    public function store(StoreSubmissionRequest $request, HomeWork $homework)
     {
-        //
+        $member_id = Member::whereRelation('student.user', 'users.id', auth()->user()->id)
+        ->whereRelation('course', 'courses.id', $homework->session->course->id)
+        ->first()->id;
+
+        $submission = $homework->submissions()->create([
+            ...$request->validated(),
+            'member_id' => $member_id
+        ]);
+
+        $files = FileService::makeFiles($request->file('files'), 'homeworks/'. $member_id);
+
+        $submission->files()->sync($files->pluck('id')->toArray());
+
+        return to_route('dashboard');
     }
 
     /**
