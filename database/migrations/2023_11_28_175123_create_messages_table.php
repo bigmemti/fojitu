dcfd26fe99eb78b51d18d116a3d977e818f52f80
Amin <151558303+AminDaneshPajuh@users.noreply.github.com>
2023-11-29 10:13:49 +0330

    ticket : add box_id to message

--- database/migrations/2023_11_28_175123_create_messages_table.php
+++ database/migrations/2023_11_28_175123_create_messages_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Box;
 use App\Models\Ticket;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -15,6 +16,7 @@ return new class extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Ticket::class)->constrained();
+            $table->foreignIdFor(Box::class)->constrained();
             $table->text("message");
             $table->timestamps();
             $table->softDeletes();
