29e60b570039b190cdbee5dd4000ccde2ace3271
stabni <stabnii83@gmail.com>
2023-12-03 01:27:25 -0800

    fix menu array

--- lang/fa/layout.php
+++ lang/fa/layout.php
@@ -17,22 +17,8 @@ return [
             ]
         ],
         [
-            'title' => __('Dashboard'),
-            'route' => route('dashboard'),
-            'active' => request()->routeIs('dashboard')
-        ],
-        [
-            'title' => __('Institutions'),
-            'route' => route('institution.index'),
-            'active' => request()->routeIs('institution.index'),
-            'policy' => [
-                'method' => 'viewAny',
-                'model' => App\Models\Institution::class
-            ]
-        ],
-        [
-            'title' => ('Majors'),
-            'route' =>('major.index') ,
+            'title' => __('Majors'),
+            'route' =>route('major.index') ,
             'active' =>request()->routeIs('major.index') ,
             'policy' => [
                 'method' => 'viewAny',
@@ -40,8 +26,8 @@ return [
             ]
         ],
         [
-            'title' => ('Curricula') ,
-            'route' =>('curriculum.index') ,
+            'title' => __('Curricula') ,
+            'route' =>route('curriculum.index') ,
             'active' =>request()->routeIs('curriculum.index') ,
             'policy' => [
                 'method' => 'viewAny',
@@ -49,44 +35,44 @@ return [
             ]
         ],
         [
-            'title' => ('Types'),
-            'route' => ('type.index'),
+            'title' => __('Types'),
+            'route' =>route ('type.index'),
             'active' => request()->routeIs('type.index'),
             'policy' => [
-                'method' => viewAny,
+                'method' => 'viewAny',
                 'model' =>App\Models\Type::class
             ]
         ],
         [
-            'title' => ('Teacher Requests'),
-            'route' => ('teacher_request.index'),
+            'title' => __('Teacher Requests'),
+            'route' =>route ('teacher_request.index'),
             'active' => request()->routeIs('teacher_request.index'),
             'policy' => [
-                'method' => viewAny,
+                'method' => 'viewAny',
                 'model' =>App\Models\TeacherRequest::class
             ]
         ],
         [
-            'title' => ('Teaching Courses'),
-            'route' => ('teacher.course.index',['teacher' => auth()->user()->teacher]),
-            'active' =>request()->routeIs('teacher.course.index',['teacher' => auth()->user()->teacher]) ,
+            'title' => __('Teaching Courses'),
+            'route' =>route ('teacher.course.index',['teacher' => auth()->user()->teacher??1]),
+            'active' =>request()->routeIs('teacher.course.index',['teacher' => auth()->user()->teacher??1 ]) ,
             'policy' => [
-                'method' => viewTeachingCourse,
+                'method' => 'viewTeachingCourse',
                 'model' =>App\Model\Course::class
             ]
         ],
         [
-            'title' => ('Studying Courses'),
-            'route' =>('curriculum.course.index',['curriculum' => auth()->user()->student->curriculum]) ,
-            'active' => request()->routeIs('curriculum.course.index',['curriculum' => auth()->user()->student->curriculum]),
+            'title' => __('Studying Courses'),
+            'route' =>route('curriculum.course.index',['curriculum' => auth()->user()->student->curriculum??1]) ,
+            'active' => request()->routeIs('curriculum.course.index',['curriculum' => auth()->user()->student->curriculum??1]),
             'policy' => [
-                'method' => viewStudyingCourse,
+                'method' => 'viewStudyingCourse',
                 'model' =>App\Model\Course::class
             ]
         ],    
             [
-                'title' => ('Courses'),
-                'route' => ('course.index'),
+                'title' => __('Courses'),
+                'route' =>route ('course.index'),
                 'active' => request()->routeIs('course.index'),
                 'policy' => [
                     'method' => 'viewAny',
